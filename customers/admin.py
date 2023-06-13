0c88a1200b2984ba0effa973d8356f58c9672d04
stevescilar <stevemuambi@gmail.com>
2023-06-13 16:33:24 +0300

    aDMIN

--- customers/admin.py
+++ customers/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Customer
 
-# Register your models here.
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'contact', 'address']
+    list_filter = ['created_at']
+    search_fields = ['name','contact']
+    
--- customers/models.py
+++ customers/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+    name = models.CharField(max_length=200)
+    contact = models.CharField(max_length=10)
+    address = models.TextField()
+    email = models.EmailField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
